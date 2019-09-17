b4b5f10f3e8fefac9499a9140b6a03efa60ea9e8
Jessica Espinoza <jessicaes2000@gmail.com>
2019-09-17 08:16:47 -0600

    Added files

---
+++ entities/enemies/BlueCrocodile.c
@@ -0,0 +1,11 @@
+//
+// Created by Jesica on 9/16/2019.
+//
+
+typedef struct BlueCrocodile{
+    int xPoss;
+    int yPoss;
+    //Sprite;
+    int width;
+    int height;
+};
---
+++ entities/players/Monkey.c
@@ -0,0 +1,15 @@
+//
+// Created by Jesica on 9/14/2019.
+//
+
+/*
+ * struct defined for player (Donkey Jr.)
+ */
+
+typedef struct Monkey{
+    int xPoss;
+    int yPoss;
+    //Sprite;
+    int width;
+    int height;
+};
