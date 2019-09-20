f7b9711a945db936077eebe6bba00c1f3db9cad4
Jessica <jessicaesp@gmail.com>
2019-09-19 22:00:54 -0600

    Added status files

---
+++ src/game/status.h
@@ -0,0 +1,17 @@
+//
+// Created by jessica on 16/09/19.
+//
+
+#ifndef DONCEY_KONG_JR_STATUS_H
+#define DONCEY_KONG_JR_STATUS_H
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include "Game.h"
+
+void runGame();
+
+
+#endif //DONCEY_KONG_JR_STATUS_H
