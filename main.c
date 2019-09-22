c2975305622e20fa4f75815f650d7f61b751a411
JoDaniel1412 <jodaniel1409@gmail.com>
2019-09-22 03:03:17 -0600

    Deleted files.

--- main.c
+++ main.c
@@ -2,12 +2,34 @@
 // Created by jessica on 16/09/19.
 //
 
-#include "SDL.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
-#include "src/game/status.h"
-
-
+#include "src/game/Game.h"
 
 int main(int argc, char *argv[]){
-    runGame();
+
+    Controller controller;
+    SDL_Window *window = NULL;
+    createWindow(window, &controller);
+
+    loadGraphics(&controller);
+
+    //Enter loop
+    int finish = 0;
+
+    //Event loop
+    while(finish != 1)
+    {
+        finish = eventManager(window, &controller);
+
+        //Render
+        render(&controller);
+        move(&controller);
+        collisionDetect(&controller);
+
+        SDL_Delay(10);
+    }
+
+    closeWindow(window, &controller);
 }
